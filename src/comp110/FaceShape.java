30ad92bfc9fa9951e650e27917126bfad7a15f10
Kris Jordan <krisjordan@gmail.com>
2017-03-04 15:57:05 -0500

    clean up comments

--- src/comp110/FaceShape.java
+++ src/comp110/FaceShape.java
@@ -20,13 +20,12 @@ public class FaceShape {
 
         // TODO: Draw your FaceShape by adding one or more shapes to aGroup.
         // Remember to add a child to aGroup: aGroup.getChildren().add(<shape>);
-        //
+
         // You will need to import the class of every shape you use.
         // For a list of classes, see the Library Class Reference at the end
         // of the write-up or view the official Java documentation:
         // https://docs.oracle.com/javase/8/javafx/api/javafx/scene/shape/package-summary.html
 
-        //
         // * Requirement: One shape in aGroup must have its fill property
         // set to the _tone instance variable.
 
